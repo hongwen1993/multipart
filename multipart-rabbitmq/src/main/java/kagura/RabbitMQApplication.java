8287076179ae333242943dcd3f306a46c6961200
hongwen <15700051127@163.com>
2019-08-01 09:01:47 +0800

    添加基础项目 方便扩展

---
+++ multipart-rabbitmq/src/main/java/kagura/RabbitMQApplication.java
@@ -0,0 +1,17 @@
+package kagura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:hongwen0928@outlook.com">Karas</a>
+ * @date 2019/8/1 8:53
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class RabbitMQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMQApplication.class, args);
+    }
+}
