4ab77e9453f375f0cbf1cc2a8603ed284ee6eb33
hongw <15700051127@163.com>
2019-08-08 11:43:16 +0800

    死信队列ok

--- multipart-rabbitmq/src/main/java/kagura/api/dlx/Consumer.java
+++ multipart-rabbitmq/src/main/java/kagura/api/dlx/Consumer.java
@@ -24,12 +24,10 @@ public class Consumer {
         // 声明第1个exchange和queue - 用来存放死信消息
         String dlxExchange = "dlx.exchange";
         String dlxRoutingKey = "#";
-        String dlxQueueName = "test_dlx_queue";
+        String dlxQueueName = "dlx.queue";
         channel.exchangeDeclare(dlxExchange, RabbitMQUtils.TOPIC);
         channel.queueDeclare(dlxQueueName, true, false, false, null);
         channel.queueBind(dlxQueueName, dlxExchange, dlxRoutingKey);
-
-
 
         // 声明第2个exchange和queue
         String exchange = "test_dlx_exchange";
@@ -39,15 +37,15 @@ public class Consumer {
         Map<String, Object> arguments = new HashMap<>();
         arguments.put("x-dead-letter-exchange", dlxExchange);
         channel.exchangeDeclare(exchange, RabbitMQUtils.TOPIC);
+        // 关联到死信队列对应的 Exchange 上
         channel.queueDeclare(queueName, true, false, false, arguments);
         channel.queueBind(queueName, exchange, routingKey);
-
 
         channel.basicConsume(queueName, true, new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
                                        byte[] body) throws IOException {
-
+                // do something...
             }
         });
     }
--- multipart-rabbitmq/src/main/java/kagura/api/dlx/Producer.java
+++ multipart-rabbitmq/src/main/java/kagura/api/dlx/Producer.java
@@ -1,10 +1,12 @@
 package kagura.api.dlx;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import kagura.utils.RabbitMQUtils;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -20,6 +22,10 @@ public class Producer {
         String exchange = "test_dlx_exchange";
         String routingKey = "dlx.save";
         String message = "I am dlx ";
-        channel.basicPublish(exchange, routingKey, true, null, message.getBytes());
+        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
+                .contentEncoding("UTF-8")
+                .expiration("15000")
+                .build();
+        channel.basicPublish(exchange, routingKey, true, properties, message.getBytes());
     }
 }
