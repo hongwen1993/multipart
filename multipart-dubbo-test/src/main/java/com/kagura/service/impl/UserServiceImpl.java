b72d7a28660436d04f0ca934710f327499785121
hongw <15700051127@163.com>
2019-08-15 11:58:07 +0800

    dubbo test

---
+++ multipart-dubbo-test/src/main/java/com/kagura/service/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kagura.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.kagura.service.UserService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author <a href="mailto:hongwen0928@outlook.com">Karas</a>
+ * @date 2019/8/15 11:21
+ * @since 1.0.0
+ */
+// 注解@Service必须用在实现类上，而不能用在接口或者抽象类上
+// 且@Service并不会将该类实例化，还是需要使用注解@Component
+@Service(interfaceClass = UserService.class)
+@Component
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public String saveUser(Long id) {
+        System.err.println("已保存用户 : " + id);
+        return "OK";
+    }
+}
