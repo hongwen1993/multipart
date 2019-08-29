ef5d9d8f621413324af7ce2083b7160f48293583
hongw <15700051127@163.com>
2019-08-29 13:18:53 +0800

    证书测试

---
+++ multipart-springboot-ssl/src/main/java/com.kagura/SSLDemoApplication.java
@@ -0,0 +1,52 @@
+package com.kagura;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author <a href="mailto:hongwen0928@outlook.com">Karas</a>
+ * @date 2019/8/1 9:00
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class SSLDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SSLDemoApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory(){
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");//机密的
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(httpConnector());
+        return factory;
+    }
+
+    @Bean
+    public Connector httpConnector(){
+        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8012);
+        return connector;
+    }
+
+}
