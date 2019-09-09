10f565adf7c04e4da54b023ba663bd099a719ceb
hongw <15700051127@163.com>
2019-09-09 16:36:47 +0800

    递归方式实现最大最小

---
+++ multipart-dubbo-producer01/src/test/java/com/kagura/arithmetic/RecurveMax.java
@@ -0,0 +1,40 @@
+package com.kagura.arithmetic;
+
+/**
+ * 递归求最大最小
+ *
+ * @author <a href="mailto:hongwen0928@outlook.com">Karas</a>
+ * @date 2019/9/9 16:30
+ * @since 1.0.0
+ */
+public class RecurveMax {
+
+    public static int max(int[] arr, int L, int R) {
+        if (L == R) {
+            return arr[L];
+        }
+        int mid = (L + R) / 2;
+        int maxLeft = max(arr, L, mid);
+        int maxRight = max(arr, mid + 1, R);
+        return Math.max(maxLeft, maxRight);
+    }
+
+    public static int min(int[] arr, int L, int R) {
+        if (L == R) {
+            return arr[L];
+        }
+        int mid = (L + R) >> 1;
+        int minLeft = min(arr, L, mid);
+        int minRight = min(arr, mid + 1, R);
+        return Math.min(minLeft, minRight);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 9, 20, 5, -6, 2, 5, 8, 0};
+        int i = max(arr, 0, arr.length - 1);
+        int j = min(arr, 0, arr.length - 1);
+        System.err.println("max : " + i);
+        System.err.println("min : " + j);
+    }
+
+}
