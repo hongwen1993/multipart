cd868c6d50907c6937db00d0fbd37cdfdf50da61
hongw <15700051127@163.com>
2019-10-08 16:14:11 +0800

    增加矩阵最小路径demo

--- multipart-elk/src/test/java/com/kagura/demo/Test02.java
+++ multipart-elk/src/test/java/com/kagura/demo/Test02.java
@@ -8,23 +8,48 @@ public class Test02 {
 
     @Test
     public void test01() {
-        BigDecimal b1 = new BigDecimal("0.1");
-        System.out.println(b1.setScale(-1, BigDecimal.ROUND_DOWN));
-        System.out.println(b1.setScale(0, BigDecimal.ROUND_DOWN));
-        System.out.println(b1.setScale(1, BigDecimal.ROUND_DOWN));
-        System.out.println(b1.setScale(2, BigDecimal.ROUND_DOWN));
-        System.out.println(b1.setScale(3, BigDecimal.ROUND_DOWN));
-        BigDecimal b2 = new BigDecimal("1.1");
-        System.out.println(b2.setScale(-1, BigDecimal.ROUND_DOWN));
-        System.out.println(b2.setScale(0, BigDecimal.ROUND_DOWN));
-        System.out.println(b2.setScale(1, BigDecimal.ROUND_DOWN));
-        System.out.println(b2.setScale(2, BigDecimal.ROUND_DOWN));
-        System.out.println(b2.setScale(3, BigDecimal.ROUND_DOWN));
-        BigDecimal b3 = new BigDecimal("1.5");
-        System.out.println(b3.setScale(-1, BigDecimal.ROUND_DOWN));
-        System.out.println(b3.setScale(0, BigDecimal.ROUND_DOWN));
-        System.out.println(b3.setScale(1, BigDecimal.ROUND_DOWN));
-        System.out.println(b3.setScale(2, BigDecimal.ROUND_DOWN));
-        System.out.println(b3.setScale(3, BigDecimal.ROUND_DOWN));
+        int[][] arr = new int[4][4];
+        arr[0][0] = 1;
+        arr[0][1] = 8;
+        arr[0][2] = 5;
+        arr[0][3] = 8;
+        arr[1][0] = 3;
+        arr[1][1] = 1;
+        arr[1][2] = 0;
+        arr[1][3] = 8;
+        arr[2][0] = 5;
+        arr[2][1] = 3;
+        arr[2][2] = 6;
+        arr[2][3] = 4;
+        arr[3][0] = 9;
+        arr[3][1] = 4;
+        arr[3][2] = 1;
+        arr[3][3] = 0;
+        System.err.println(getMin(arr));
+
     }
+
+    // 动态规划
+    public int getMin(int[][] arr) {
+        if (arr == null || arr.length == 0 || arr[0] == null || arr[0].length == 0) {
+            return 0;
+        }
+        int row = arr.length;
+        int col = arr[0].length;
+        int[][] dp = new int[row][col];
+        dp[0][0] = arr[0][0];
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = dp[i - 1][0] + arr[i][0];
+        }
+        for (int i = 1; i < col; i++) {
+            dp[0][i] = dp[0][i - 1] + arr[0][i];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + arr[i][j];
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+
 }
