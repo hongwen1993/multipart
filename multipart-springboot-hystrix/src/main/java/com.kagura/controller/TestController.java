ddda757629c644ac7cf92a4934a8292f576f1244
hongw <15700051127@163.com>
2019-10-21 18:16:23 +0800

    test

--- multipart-springboot-hystrix/src/main/java/com.kagura/controller/TestController.java
+++ multipart-springboot-hystrix/src/main/java/com.kagura/controller/TestController.java
@@ -4,6 +4,8 @@ import com.kagura.service.TestHystrixCommand;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 /**
  * @author <a href="mailto:hongwen0928@outlook.com">Karas</a>
  * @date 2019/8/12 17:57
